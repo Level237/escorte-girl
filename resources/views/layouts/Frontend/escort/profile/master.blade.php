8edf1a288bc6df5f1ca9070643c4883a1108bd4b
level237 <Bramslevel129@gmail.com>
2023-11-03 10:52:40 +0100

    step-one:profil escort init

--- resources/views/layouts/Frontend/escort/profile/master.blade.php
+++ resources/views/layouts/Frontend/escort/profile/master.blade.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://unpkg.com/tailwindcss@1.3.4/dist/tailwind.min.css" rel="stylesheet">
-    <title>@yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="h-screen overflow-hidden flex items-center justify-center" style="background: #edf2f7;">
-@yield('content')
-</div>
+<body class="h-screen overflow-hidden" style="background: #111827">
+   @yield('content')
+
 </body>
 </html>
