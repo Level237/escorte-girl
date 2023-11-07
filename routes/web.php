46bc7cad35bba981c62c31335736a60e519ea6d8
level237 <Bramslevel129@gmail.com>
2023-11-07 11:59:08 +0100

    fixed auth middleware

--- routes/web.php
+++ routes/web.php
@@ -60,15 +60,7 @@ Route::post('/ads',[AdsController::class, 'save'])->name('ads.save');
 
 Route::post('ads/image',[AdsImageController::class, 'images'])->name('ads.image');
 
-Route::prefix('dashboard')->group(function () {
-    Route::get('index', [DashboardEscortController::class, 'index'])->name('db.escort.index');
-    Route::get('profil', [DashboardEscortController::class, 'profil'])->name('db.escort.profil');
-    Route::get('ads', [DashboardEscortController::class, 'ads'])->name('db.escort.ads');
-    Route::get('messages', [DashboardEscortController::class, 'messages'])->name('db.escort.messages');
-    Route::get('finance', [DashboardEscortController::class, 'finance'])->name('db.escort.finance');
-    Route::get('advertise', [DashboardEscortController::class, 'advertise'])->name('db.escort.advertise');
-    Route::get('settings', [DashboardEscortController::class, 'settings'])->name('db.escort.settings');
-});
+
 
 
 //Route with auth middleware
@@ -80,5 +72,14 @@ Route::middleware(['escort'])->group(function () {
     Route::get('/step-three',[StepThreeController::class,'stepThree'])->name('step-three');
     Route::post('/step-three-store',[StepThreeController::class,'stepThreeStore'])->name('step-three-store');
     Route::get('/step-final',[StepFinalController::class,'stepFinal'])->name('step.final');
+    Route::prefix('dashboard')->group(function () {
+        Route::get('index', [DashboardEscortController::class, 'index'])->name('db.escort.index');
+        Route::get('profil', [DashboardEscortController::class, 'profil'])->name('db.escort.profil');
+        Route::get('ads', [DashboardEscortController::class, 'ads'])->name('db.escort.ads');
+        Route::get('messages', [DashboardEscortController::class, 'messages'])->name('db.escort.messages');
+        Route::get('finance', [DashboardEscortController::class, 'finance'])->name('db.escort.finance');
+        Route::get('advertise', [DashboardEscortController::class, 'advertise'])->name('db.escort.advertise');
+        Route::get('settings', [DashboardEscortController::class, 'settings'])->name('db.escort.settings');
+    });
 });
 
