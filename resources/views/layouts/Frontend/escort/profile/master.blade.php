30a1eb2b47a698dd2f0bf74e87df9e312477c026
level237 <Bramslevel129@gmail.com>
2023-11-10 10:27:34 +0100

    update

--- resources/views/layouts/Frontend/escort/profile/master.blade.php
+++ resources/views/layouts/Frontend/escort/profile/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://unpkg.com/tailwindcss@1.3.4/dist/tailwind.min.css" rel="stylesheet">
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
 </head>
 <body class="h-screen " style="background: #111827">
    @yield('content')
