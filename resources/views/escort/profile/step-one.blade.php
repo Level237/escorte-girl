ade21b3e3d977e8501609523a54aa38b00384d87
Frank Zohim <delanofofe@gmail.com>
2023-11-10 12:18:15 +0100

    commit

--- resources/views/escort/profile/step-one.blade.php
+++ resources/views/escort/profile/step-one.blade.php
@@ -56,7 +56,7 @@
                     </div>
                     <div class="w-full flex-1 mx-2 svelte-1l8159u">
                         <div class="mb-6">
-                            <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 text-white">Email</label>
                             <input name="email"  type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         </div>
 
@@ -66,7 +66,7 @@
                 </div>
                 <div class="flex flex-col md:flex-row">
                     <div class="w-full flex-1 mx-2 svelte-1l8159u">
-                        <label for="age" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quel age aviez vous? *</label>
+                        <label for="age" class="block mb-2 text-sm font-medium text-gray-900 text-white">Quel age aviez vous? *</label>
                         <select id="age" name="age" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <option value="">Selectionnez un age</option>
                             <option>18</option>
