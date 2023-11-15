9a496b3b0a46fea27d212cfadcb43bdcfe583f2d
level237 <Bramslevel129@gmail.com>
2023-11-15 23:10:29 +0100

    init add user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\User\ActivateUserController;
+use App\Http\Controllers\Admin\User\AddUserController;
 use App\Http\Controllers\Admin\User\ListUserController;
 use App\Http\Controllers\Admin\User\SuspendUserController;
 use Illuminate\Support\Facades\Route;
@@ -127,6 +128,7 @@ Route::middleware(['admin'])->prefix('admin')->group(function () {
 
    Route::get('dashboard',[DashboardAdminController::class,'index'])->name('admin.dashboard');
    Route::get('users',[ListUserController::class,'listUser'])->name('users');
+   Route::get('users/create',[AddUserController::class,'addView'])->name('users.create');
    Route::get('users/suspend',[ListUserController::class,'listUserBan'])->name('users.suspend');
    Route::get('users/role/',[ListUserController::class,'userByRole'])->name('user-by-role');
    Route::get('suspend/user/{id}',[SuspendUserController::class,'ban'])->name('suspend-user');
