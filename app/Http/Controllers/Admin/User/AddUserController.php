3b6f7b28c1881b78c92607c304c02a8415bebdb7
level237 <Bramslevel129@gmail.com>
2023-11-17 05:40:51 +0100

    update

--- app/Http/Controllers/Admin/User/AddUserController.php
+++ app/Http/Controllers/Admin/User/AddUserController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\Api\Admin\AllUserService;
 
 class AddUserController extends Controller
 {
     public function addView(){
-
-        return view('backend.users.create');
+        $roles=(new AllUserService())->roles();
+        return view('backend.users.create',compact('roles'));
     }
 }
--- resources/views/backend/users/create.blade.php
+++ resources/views/backend/users/create.blade.php
@@ -37,136 +37,53 @@ Nouvel utilisateur
 
                             <div class="tab-content">
                                 <div class="tab-pane show active" id="input-types-preview">
-                                    <div class="row">
-                                        <div class="col-lg-6">
+
+
                                             <form>
-                                                <div class="mb-3">
-                                                    <label for="simpleinput" class="form-label">Nom utilisateur</label>
-                                                    <input type="text" name="username" id="simpleinput" class="form-control">
+                                                <div class="row">
+                                                <div class="col-lg-12">
+                                                    <div class="mb-3">
+                                                        <label for="simpleinput" class="form-label">Nom utilisateur</label>
+                                                        <input type="text" name="username" id="simpleinput" class="form-control">
+                                                    </div>
                                                 </div>
 
+                                            </div>
                                                 <div class="mb-3">
                                                     <label for="example-email" class="form-label">Email</label>
-                                                    <input type="email" id="example-email" name="example-email" class="form-control" placeholder="Email">
+                                                    <input type="email" id="example-email" name="email" class="form-control" placeholder="Email">
                                                 </div>
-
                                                 <div class="mb-3">
-                                                    <label for="example-password" class="form-label">Password</label>
-                                                    <input type="password" id="example-password" class="form-control" value="password">
+                                                    <label for="example-email" class="form-label">Numéro de Téléphone</label>
+                                                    <input type="text" id="phone_number" name="phone_number" class="form-control" placeholder="Email">
                                                 </div>
-
                                                 <div class="mb-3">
-                                                    <label for="password" class="form-label">Show/Hide Password</label>
+                                                    <label for="password" class="form-label">Mot de passe</label>
                                                     <div class="input-group input-group-merge">
-                                                        <input type="password" id="password" class="form-control" placeholder="Enter your password">
+                                                        <input name="password" type="password" id="password" class="form-control" placeholder="Entrez le mot de passe de l'utilisateur">
                                                         <div class="input-group-text" data-password="false">
                                                             <span class="password-eye"></span>
                                                         </div>
                                                     </div>
                                                 </div>
 
-                                                <div class="mb-3">
-                                                    <label for="example-palaceholder" class="form-label">Placeholder</label>
-                                                    <input type="text" id="example-palaceholder" class="form-control" placeholder="placeholder">
-                                                </div>
+                                                    <div class="mb-3">
+                                                        <label for="example-select" class="form-label">Role</label>
+                                                        <select name="role_id" class="form-select" id="example-select">
+                                                            @foreach ($roles as $role)
+                                                            @foreach ($role as $r)
+                                                            <option value="{{ $r->id }}">{{ $r->role_name }}</option>
+                                                            @endforeach
 
-                                                <div class="mb-3">
-                                                    <label for="example-textarea" class="form-label">Text area</label>
-                                                    <textarea class="form-control" id="example-textarea" rows="5"></textarea>
-                                                </div>
+                                                            @endforeach
+                                                        </select>
 
-                                                <div class="mb-3">
-                                                    <label for="example-readonly" class="form-label">Readonly</label>
-                                                    <input type="text" id="example-readonly" class="form-control" readonly="" value="Readonly value">
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="example-disable" class="form-label">Disabled</label>
-                                                    <input type="text" class="form-control" id="example-disable" disabled="" value="Disabled value">
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="example-static" class="form-label">Static control</label>
-                                                    <input type="text" readonly class="form-control-plaintext" id="example-static" value="email@example.com">
-                                                </div>
-
-                                                <div class="mb-0">
-                                                    <label for="example-helping" class="form-label">Helping text</label>
-                                                    <input type="text" id="example-helping" class="form-control" placeholder="Helping text">
-                                                    <span class="help-block"><small>A block of help text that breaks onto a new line and may extend beyond one line.</small></span>
-                                                </div>
+                                                    </div>
 
                                             </form>
-                                        </div> <!-- end col -->
 
-                                        <div class="col-lg-6">
-                                            <form>
 
-                                                <div class="mb-3">
-                                                    <label for="example-select" class="form-label">Input Select</label>
-                                                    <select class="form-select" id="example-select">
-                                                        <option>1</option>
-                                                        <option>2</option>
-                                                        <option>3</option>
-                                                        <option>4</option>
-                                                        <option>5</option>
-                                                    </select>
-                                                </div>
 
-                                                <div class="mb-3">
-                                                    <label for="example-multiselect" class="form-label">Multiple Select</label>
-                                                    <select id="example-multiselect" multiple class="form-control">
-                                                        <option>1</option>
-                                                        <option>2</option>
-                                                        <option>3</option>
-                                                        <option>4</option>
-                                                        <option>5</option>
-                                                    </select>
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="example-fileinput" class="form-label">Default file input</label>
-                                                    <input type="file" id="example-fileinput" class="form-control">
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="example-date" class="form-label">Date</label>
-                                                    <input class="form-control" id="example-date" type="date" name="date">
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="example-month" class="form-label">Month</label>
-                                                    <input class="form-control" id="example-month" type="month" name="month">
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="example-time" class="form-label">Time</label>
-                                                    <input class="form-control" id="example-time" type="time" name="time">
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="example-week" class="form-label">Week</label>
-                                                    <input class="form-control" id="example-week" type="week" name="week">
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="example-number" class="form-label">Number</label>
-                                                    <input class="form-control" id="example-number" type="number" name="number">
-                                                </div>
-
-                                                <div class="mb-3">
-                                                    <label for="example-color" class="form-label">Color</label>
-                                                    <input class="form-control" id="example-color" type="color" name="color" value="#727cf5">
-                                                </div>
-
-                                                <div class="mb-0">
-                                                    <label for="example-range" class="form-label">Range</label>
-                                                    <input class="form-range" id="example-range" type="range" name="range" min="0" max="100">
-                                                </div>
-
-                                            </form>
-                                        </div> <!-- end col -->
-                                    </div>
                                     <!-- end row-->
                                 </div> <!-- end preview-->
 
