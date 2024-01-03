010868a8a372571f03ead9297d965b19319e739b
level237 <Bramslevel129@gmail.com>
2024-01-03 22:43:48 +0100

    update

--- app/Http/Controllers/Ads/AdsController.php
+++ app/Http/Controllers/Ads/AdsController.php
@@ -198,41 +198,42 @@ class AdsController extends Controller
             $allAds = json_decode((string) $response->getBody(), true);
             $allAds = $allAds['data'];
             $towns=(new ListTownService())->list()->data;
+            if($request->membership){
+                $adsByMembership = [];
+                $i = 0;
+                foreach($allAds as $ad){
+                    if($ad['subscribe_id'] == $request->membership){
+                        $adsByMembership[$i] = $ad;
+                        $i++;
+                    }
+                }
+               $allAds =  $adsByMembership;
+            }
+
+
+            $total = count($allAds);
+            $per_page = 6;
+            $nb_pages = ceil($total/$per_page);
+            $current_page = $request->current_page ?? 1;
+
+            $starting_point = ($current_page * $per_page) - $per_page;
+
+            $ads = array_slice($allAds, $starting_point, $per_page, true);
+
+            $ads = new Paginator($ads, $total, $per_page, $current_page, [
+                'path' => $request->url(),
+                'query' => $request->query(),
+            ]);
+
+            //dd($allAds);
+
+            return  view('ads.list', compact('ads', 'allAds', 'current_page', 'nb_pages','towns'));
             //error_log($ads);
         }catch(\Exception $e){
              $allAds = [];
         }
 
-        if($request->membership){
-            $adsByMembership = [];
-            $i = 0;
-            foreach($allAds as $ad){
-                if($ad['subscribe_id'] == $request->membership){
-                    $adsByMembership[$i] = $ad;
-                    $i++;
-                }
-            }
-           $allAds =  $adsByMembership;
-        }
 
-
-        $total = count($allAds);
-        $per_page = 6;
-        $nb_pages = ceil($total/$per_page);
-        $current_page = $request->current_page ?? 1;
-
-        $starting_point = ($current_page * $per_page) - $per_page;
-
-        $ads = array_slice($allAds, $starting_point, $per_page, true);
-
-        $ads = new Paginator($ads, $total, $per_page, $current_page, [
-            'path' => $request->url(),
-            'query' => $request->query(),
-        ]);
-
-        //dd($allAds);
-
-        return  view('ads.list', compact('ads', 'allAds', 'current_page', 'nb_pages','towns'));
     }
 
 
