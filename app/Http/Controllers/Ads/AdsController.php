c99f8b2901855bc83372f2650de24267b20a33a8
Frank Zohim <delanofofe@gmail.com>
2024-02-21 08:53:55 +0100

    some updates

--- app/Http/Controllers/Ads/AdsController.php
+++ app/Http/Controllers/Ads/AdsController.php
@@ -61,9 +61,10 @@ class AdsController extends Controller
         //Retrieve URL API
         $url=(new UrlApiService())->getUrl();
 
+        
         //Saving Ads
         $token=Session::get('tokenUser');
-
+     
         $isVideo = false;
         if($request->file('video') != null){
             $isVideo = true;
@@ -148,6 +149,7 @@ class AdsController extends Controller
 
         else{
 
+               
             try{
 
                         $response = Http::withToken($token)->post($url."/api/v1/ads", [
@@ -157,7 +159,6 @@ class AdsController extends Controller
                              'gender' => $request->gender,
                              'age' => $request->age,
                              'phone' => $request->phone,
-                             'services' => $request->services,
                              'category_id' => $request->category,
                              'accepted' => $request->accepted,
                              'title' => $request->title,
@@ -167,7 +168,7 @@ class AdsController extends Controller
 
 
 
-                //dd($response);
+                //dd($response->body());
                 //dd(json_decode((string) $response->getBody(), true));
                 if($response->status() === 200){
 
